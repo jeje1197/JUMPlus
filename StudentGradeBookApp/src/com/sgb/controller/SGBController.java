be840b834602fc03bf7c48116b3b3138c78ba879
Joseph E <1jojorun@gmail.com>
2023-04-17 15:35:02 -0700

    StudentGradeBookApp: Setup project structure

--- StudentGradeBookApp/src/com/sgb/controller/SGBController.java
+++ StudentGradeBookApp/src/com/sgb/controller/SGBController.java
@@ -1,50 +1,77 @@
 package com.sgb.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.sgb.exception.InvalidInputException;
+import com.sgb.model.SchoolClass;
+import com.sgb.model.Teacher;
 import com.sgb.utils.ColorPrinter;
 import com.sgb.utils.ConsolePrinter;
 import com.sgb.utils.ConsoleScanner;
 import com.sgb.utils.PrettyFormatter;
 
 public class SGBController {
+	private static List<SchoolClass> classes = new ArrayList<>();
+	private static List<Teacher> teachers = new ArrayList<>();
+	private static Teacher currentAccount = null;
+
 
 	public static void mainMenu() {
-			String[] menuOptions = {
-					"1. REGISTER",
-					"2. LOGIN",
-					"3. EXIT"
-			};
+		String[] menuOptions = {
+				"1. REGISTER",
+				"2. LOGIN",
+				"3. EXIT"
+		};
 
-			boolean applicationShouldRun = true;
-			while (applicationShouldRun) {
-				ColorPrinter.println(ColorPrinter.CYAN, PrettyFormatter.format(menuOptions));
+		boolean applicationShouldRun = true;
+		while (applicationShouldRun) {
+			ColorPrinter.println(ColorPrinter.CYAN, PrettyFormatter.format(menuOptions));
 
-				int selectedOption = -1;
-				try {
-					ColorPrinter.print(ColorPrinter.GREEN, "Choose an option (1-4): ");
-					selectedOption = ConsoleScanner.readInt(1, 4);
-					ConsolePrinter.println("");
-				} catch (InvalidInputException e) {
-					ColorPrinter.println(ColorPrinter.RED, e.getMessage() + "\n");
-					continue;
-				}
-
-				switch (selectedOption) {
-				case 1:
-					break;
-
-				case 2:
-					break;
-
-				case 3:
-					break;
-
-				case 4:
-					applicationShouldRun = false;
-				}
+			int selectedOption = -1;
+			try {
+				ColorPrinter.print(ColorPrinter.GREEN, "Choose an option (1-3): ");
+				selectedOption = ConsoleScanner.readInt(1, 3);
+				ConsolePrinter.println("");
+			} catch (InvalidInputException e) {
+				ColorPrinter.println(ColorPrinter.RED, e.getMessage() + "\n");
+				continue;
 			}
 
+			switch (selectedOption) {
+			case 1:
+				SGBController.register();
+				break;
 
-			ConsoleScanner.close();
+			case 2:
+				SGBController.login();
+				if (SGBController.isLoggedIn()) {
+					SGBController.teacherMenu();
+				}
+				break;
+
+			case 3:
+				applicationShouldRun = false;
+				break;
+			}
+		}
+
+		ConsoleScanner.close();
+	}
+
+	private static void register() {
+
+	}
+
+	private static void login() {
+
+	}
+
+	private static boolean isLoggedIn() {
+		return currentAccount != null;
+	}
+
+	private static void teacherMenu() {
+
 	}
 }
---
+++ StudentGradeBookApp/src/com/sgb/model/SchoolClass.java
@@ -0,0 +1,5 @@
+package com.sgb.model;
+
+public class SchoolClass {
+
+}
