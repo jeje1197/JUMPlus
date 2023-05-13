9d08dc43b87c17902634dddbdc6092f7ce167c39
Joseph E <1jojorun@gmail.com>
2023-05-13 13:44:35 -0700

    Implemented deposit/withdraw functionality

--- DollarsBankJSApp/front-end/js/account.js
+++ DollarsBankJSApp/front-end/js/account.js
@@ -9,6 +9,10 @@ function preload() {
     }
 }
 
+function storeProgramState() {
+    localStorage.setItem('programMemory', JSON.stringify(programMemory))
+}
+
 function updateDisplay() {
     const welcomeHeader = document.getElementById('account-welcome')
     const balanceHeader = document.getElementById('account-balance')
@@ -26,20 +30,68 @@ function updateDisplay() {
     } else {
         transactionList.innerHTML = `<li class="list-group-item"> No Transactions </li>`
     }
+
+    // Load customer information in modal
+    const customerNameDisplay = document.getElementById('customer-name')
+    const customerEmailDisplay = document.getElementById('customer-email')
+    const customerUsernameDisplay = document.getElementById('customer-username')
+
+    customerNameDisplay.innerText = programMemory.currentAccount.name
+    customerEmailDisplay.innerText = programMemory.currentAccount.email
+    customerUsernameDisplay.innerText = programMemory.currentAccount.username
 }
 
 function setupOnclickListeners() {
+    const depositSubmitButton = document.getElementById('deposit-submit-button')
+    const withdrawSubmitButton = document.getElementById('withdraw-submit-button')
     const logoutButton = document.getElementById('logout-button')
 
+    depositSubmitButton.onclick = deposit
+    withdrawSubmitButton.onclick = withdraw
     logoutButton.onclick = logout
+}
+
+// function loadCustomerInfo() {
+//     const customerNameDisplay = document.getElementById('customer-name')
+//     const customerEmailDisplay = document.getElementById('customer-email')
+
+//     customerNameDisplay.innerText = programMemory.currentAccount.name
+//     customerEmailDisplay.innerText = programMemory.currentAccount.email
+// }
+
+function deposit(event) {
+    event.preventDefault()
+    const depositAmount = Number(document.getElementById('deposit-amount').value)
+    
+    if (depositAmount <= 0) {
+        alert('Cannot make deposit. Deposit amount must be greater than $0.00')
+    } else {
+        programMemory.currentAccount.balance += depositAmount
+        storeProgramState()
+        alert(`Successfully deposited ${depositAmount}!`)
+        window.location.reload()
+    }
+}
+
+function withdraw(event) {
+    event.preventDefault()
+    const withdrawAmount = Number(document.getElementById('withdraw-amount').value)
+    
+    if (withdrawAmount <= 0 || withdrawAmount > programMemory.currentAccount.balance) {
+        alert(`Cannot make withdrawl. Withdrawl amount must be greater than $0.00 and less than ${programMemory.currentAccount.balance}`)
+    } else {
+        programMemory.currentAccount.balance -= withdrawAmount
+        storeProgramState()
+        alert(`Successfully deposited ${withdrawAmount}!`)
+        window.location.reload()
+    }
 }
 
 function logout() {
     programMemory.currentAccount = undefined
-    localStorage.setItem('programMemory', JSON.stringify(programMemory))
+    storeProgramState()
     window.location.href = "./index.html"
 }
-
 
 // Call functions
 preload()
