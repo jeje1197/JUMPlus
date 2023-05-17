dc59155afbad7a23e1991465474258b1dbd8fbfd
Joseph E <1jojorun@gmail.com>
2023-05-17 10:03:10 -0700

    Adding header component to web page

---
+++ FurnitureEcommerceApp/front-end/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route exact path="/" element={ <Home /> }/>
+          {/* <Route exact path="/" element={ <Home /> }/> */}
+
+
+        </ Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ FurnitureEcommerceApp/front-end/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Categories
+      Carousel Item
+    </div>
+  )
+}
+
+export default Home
