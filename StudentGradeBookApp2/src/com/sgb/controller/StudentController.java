3b86d592d5800f56cd28b61ad5e263c31c5e950a
Joseph E <1jojorun@gmail.com>
2023-07-26 11:55:55 -0500

    StudentGradeBookApp2: Implemented view classes functionality for students.

--- StudentGradeBookApp2/src/com/sgb/controller/StudentController.java
+++ StudentGradeBookApp2/src/com/sgb/controller/StudentController.java
@@ -1,16 +1,47 @@
 package com.sgb.controller;
 
+import java.io.Console;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.sgb.dao.StudentDaoSql;
+import com.sgb.exception.InvalidInputException;
 import com.sgb.model.StudentClass;
 import com.sgb.model.StudentUser;
+import com.sgb.utils.ColorPrinter;
+import com.sgb.utils.ConsoleScanner;
+import com.sgb.utils.PrettyFormatter;
+import com.sgb.utils.RegexManager;
 
 public class StudentController {
 	
 	public static void displayInformation(StudentDaoSql studentDao, StudentUser student) {
 		List<StudentClass> classes = studentDao.getStudentClasses(student);
+	
+		List<String> display = new ArrayList<>();
 		
-		System.out.println(classes);
+		display.add("Student Portal");
+		display.add("  Id: " + student.getStudentId() + "   Name: " + student.getName());
+		display.add("");
+		display.add("Your Classes: ");
+		display.add("");
+		
+		classes.forEach(studentClass -> {
+			display.add(studentClass.getClassName());
+			display.add("  Grade: " + studentClass.getGrade() + 
+					"%   Teacher: " + studentClass.getTeacherName());
+		});
+		
+		display.add("");
+		display.add("Press ENTER to return...");
+		
+		ColorPrinter.println(ColorPrinter.CYAN, 
+				PrettyFormatter.format(display.toArray(String[]::new)));
+		
+		try {
+			ConsoleScanner.readString(RegexManager.ANY, "");
+		} catch (InvalidInputException e) {
+			ColorPrinter.println(ColorPrinter.RED, e.getMessage() + "\n");
+		}
 	}
 }
